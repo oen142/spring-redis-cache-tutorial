08b7d2aac7ac7385c36fa65d6e5db3b38171e672
oen142 <oen142@naver.com>
2021-03-25 20:00:58 +0900

    feat : 레디스를 활용한 포스팅 튜토리얼 첫 이닛

---
+++ src/main/java/com/wani/springrediscachetutorial/SpringRedisCacheTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.springrediscachetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisCacheTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/springrediscachetutorial/SpringRedisCacheTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.springrediscachetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisCacheTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
