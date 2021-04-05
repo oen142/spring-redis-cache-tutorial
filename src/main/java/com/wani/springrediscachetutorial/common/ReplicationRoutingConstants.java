7fefbaaee67ac8aa5eb1869b11f41b3f2b62719e
oen142 <oen142@naver.com>
2021-04-05 23:38:43 +0900

    feat : 데이터베이스 이중화를 위해서 slave는 read replica로 사용한다.

---
+++ src/main/java/com/wani/springrediscachetutorial/common/ReplicationRoutingConstants.java
@@ -0,0 +1,9 @@
+package com.wani.springrediscachetutorial.common;
+
+public class ReplicationRoutingConstants {
+
+    public static final String MASTER = "master";
+    public static final String SLAVE = "slave";
+
+    private ReplicationRoutingConstants() {}
+}
